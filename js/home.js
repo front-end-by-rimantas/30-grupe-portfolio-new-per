d2acb959923eed2b5fc4373b5e977c0546187400
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-21 01:47:44 +0300

    Created javascript for autotype efect on Hero

--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
 // import JS modules
+import { autotypeTextData } from "./data/hero/autotypeTextData.js";
+import { AutotypeText } from "./components/hero/autotypeText.js";
 
 // execute JS modules
+new AutotypeText(autotypeTextData);
