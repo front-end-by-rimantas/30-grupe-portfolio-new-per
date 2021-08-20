818e93733f576a8df00f043b29bb2590b5a98607
Jevgenijus <j.suchodolskis@gmail.com>
2021-08-20 15:45:24 +0300

    Data update

---
+++ js/data/skillsProgressData.js
@@ -0,0 +1,35 @@
+const skillsProgressData = [
+    {
+        label: `Photography`,
+        value: 75,
+    },
+    {
+        label: `Branding`,
+        value: 95,
+    },
+    {
+        label: `Graphic Design`,
+        value: 70,
+    },
+    {
+        label: `Development`,
+        value: 80,
+    },
+    {
+        label: `Html / Css`,
+        value: 95,
+    },
+    {
+        label: `Js / Jquery`,
+        value: 80,
+    },
+    {
+        label: `Php / WorldPress`,
+        value: 75,
+    },    {
+        label: `Photo shop`,
+        value: 94,
+    },
+
+
+];
