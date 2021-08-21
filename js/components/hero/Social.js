b33eb9ca302db34410dd4a63e9b3e166ecdb7ab0
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-21 10:07:15 +0300

    Added sicial icons rendering with javascript

---
+++ js/components/hero/Social.js
@@ -0,0 +1,19 @@
+class Social {
+    constructor(className, data) {
+        this.data = data;
+        this.className = className;
+        this.render();
+    }
+    render() {
+        let html = '';
+        const socialDom = document.querySelector(`${this.className}`);
+        for (const {status, icon, link} of this.data) {
+            if (status === 'draft') continue;
+            html += `<a href="${link}" class="fa fa-${icon}"></a>\n`;
+        }
+        socialDom.innerHTML = html;
+        console.log(html);
+    }
+};
+
+export { Social };
