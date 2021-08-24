14a46591c38eb6961d29669f6d7e77c655b767d7
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-24 18:21:14 +0300

    Created javascript for blog data renderingFromFile

--- js/home.js
+++ js/home.js
@@ -1,9 +1,23 @@
-// import JS modules
+// ************ IMPORT JS MOUDLES ************
+
+// HERO - HEADER
 import { autotypeTextData } from "./data/hero/autotypeTextData.js";
 import { AutotypeText } from "./components/hero/autotypeText.js";
 import { socialData } from "./data/hero/socialData.js";
 import { Social } from "./components/hero/Social.js";
 
-// execute JS modules
+// BLOG
+import { Blog} from "./components/blog/Blog.js";
+import { blogData } from "./data/blog/blogData.js";
+
+
+
+
+// ************ EXECUTE IS MODULES ************
+
+// HERO - HEADER
 new AutotypeText(autotypeTextData);
 new Social('.socials', socialData);
+
+// BLOG
+new Blog(blogData);
