96e4d0e3ff5de036d1a77f9f2923c2da8d5e1dde
Jevgenijus <j.suchodolskis@gmail.com>
2021-08-24 16:19:31 +0300

    Skills data + fake data cheaks #3

--- js/data/skillsProgressData.js
+++ js/data/skillsProgressData.js
@@ -30,6 +30,53 @@ const skillsProgressData = [
         label: `Photo shop`,
         value: 94,
     },
+    {
+        label: 'corupted data',
+        value: -90,
+    },
+    {
+        label: 'corupted data',
+        value: Infinity,
+    },
+    {
+        label: 'corupted data',
+        value: -Infinity,
+    },
+    {
+        label: 'corupted data',
+        value: NaN,
+    },
+    {
+        label: 'corupted data',
+        value: 990,
+    },
+    {},
+    {
+        label: 'labas :P',
+    },
+    {
+        value: 'labas :P',
+    },
+    5847152,
+    {
+        label: [1, 2, 5],
+        value: 'wewrt',
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ];
 
 export { skillsProgressData }
