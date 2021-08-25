b5974f7be1329ae61e8900749d1ed3112e18b859
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-25 14:36:08 +0300

    Creating animation for testimonials

--- js/components/testimonial/Testimonial.js
+++ js/components/testimonial/Testimonial.js
@@ -3,23 +3,33 @@ class Testimonial {
         this.data = data;
         this.fullHtml = '';
         this.renderLinks();
+        this.renderFirst();
         this.listener();
         // this.renderContent();
     }
+
+    renderFirst() {
+        const navItem = document.querySelector('.nav-line');
+        let testimonialId = 0;
+        for (const testimonial of this.data) {
+            
+            if (testimonial.status === 'published') {
+                this.renderContent(testimonialId);
+                navItem.classList.add('clicked');
+                break;
+            }
+            testimonialId++;
+        }
+    }
+
+    // Renders testimonial html code from  data in external file and renders 
+    // only exact testimonial, which was clicked in navigation
     renderContent(testimonialId) {
         const tetimonialContentDOM = document.querySelector('.testimonial .content');
-        // const linkDOM = document.querySelector('.nav-line');
-        // const testimonialDOM = document.querySelector('.testimonial');
 
-        // testimonialDOM.innerHTML = this.fullHtml;
-
-        // linkDOM.addEventListener('click', function() {
-        //     console.log('CLICKED');
-        // });
 
         console.log(this.data[testimonialId]);
-        // let html = '';
-        // for (const testimonial of this.data) {
+
             const {
                 status,
                 title,
@@ -29,20 +39,17 @@ class Testimonial {
                 profession,
             } = this.data[testimonialId];
 
-            // if (status === 'published') {
-            //     testimonialCount++
-            // } else {
-            //     continue;
-            // }
 
             let ratingHtml = '';
             for (let star = 1; star <= rating; star++) {
                 ratingHtml += `<i class="fa fa-star"></i>\n`;
             }
             const html = `
+                    <div class="testimonial-animation">
                         <h1 class="title">
                             ${title}
                         </h1>
+                    </div>
                         <div class="rating">
                             ${ratingHtml}
                         </div>
@@ -57,8 +64,9 @@ class Testimonial {
                         </h3>
             `;
             tetimonialContentDOM.innerHTML = html;
-        // }
     }
+
+    // Renders all testimonial navigation line-like buttons
     renderLinks() {
         const navDOM = document.querySelector('.testimonial-nav');
         
@@ -67,21 +75,31 @@ class Testimonial {
         for (const testimonial of this.data) {
             
             if (testimonial.status === 'published') {
+                // Builds testimonial navigation sections with line-like buttons
                 navDOM.innerHTML += `<li value = "${testimonialId}" class="nav-line"></li>\n`
             }
             testimonialId++;
         }
-        // console.log(navigationHtml);
-        // this.fullHtml += navigationHtml;
-        // navDOM.innerHTML = navigationHtml;
     }
+
+    // Creates click listening events for every line-like button from array of buttons 
+    // Main purpose is to check which exactly line-like button was clicked and based on that 
+    // call testimonial rendering method with exact testimonial ID
+    // Second purpose is to add special class on clicked button to make it wider and with special color
     listener() {
         const navList = document.querySelectorAll('.nav-line');
-        console.log(navList);
+
+        // console.log(navList);
         for (const navItem of navList) {
             navItem.addEventListener('click', () => {
                 // const navID = '' + navItem;
-                console.log('clicked', navItem.value);
+                // console.log('clicked', navItem.value);
+
+                for (const navItem2 of navList) {
+                    navItem2.classList.remove('clicked');
+                }
+
+                navItem.classList.add('clicked');
                 this.renderContent(navItem.value);
             });
         }
