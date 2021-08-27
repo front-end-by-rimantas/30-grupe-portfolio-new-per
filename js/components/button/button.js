93cb833272270e0865a47ee93e04d8451348ce3b
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-27 19:47:17 +0300

    Added comments to js #3

--- js/components/button/button.js
+++ js/components/button/button.js
@@ -1,47 +1,35 @@
+// This script is for button background animation. It covers both the effect on hover and the
+// effect on out of hover. 
+
 const button = document.getElementById('new-per-button');
-// const textAnimation = document.querySelector('.main-button');
-// const backgroundAnimation = document.querySelector('.button-overlay');
 let buttonText = button.innerHTML;
 
 function buttonAnimation() {
     buttonText = buttonText.toUpperCase();
 
-
-    console.log(buttonText);
+    // Generates button html
     button.innerHTML = `
     <div id="main-button" class="main-button">
-        <div id="button-overlay" class="button-overlay">
-            
+        <div id="button-overlay" class="button-overlay"> 
         </div>
         ${buttonText}
     </div>
     `
+    // DOM variables for text and background animation
     const textAnimation = document.getElementById('main-button'); 
-    // querySelector('.main-button');
     const backgroundAnimation = document.getElementById('button-overlay');
-    // querySelector('.button-overlay');
-console.log(textAnimation, backgroundAnimation);
 
-
+    // Animation for hover begins. Button mouseenter listener
     button.addEventListener('mouseenter', () => {
-        console.log('enterdd');
         textAnimation.classList.add('text-animation');
         backgroundAnimation.classList.add('background-animation');
-        // button.innerHTML = `
-        // <div class="main-button text-animation">
-        //     <div class="button-overlay background-animation">
-                
-        //     </div>
-        //     ${buttonText}
-        // </div>
-        // `
-
 
     setTimeout(() => {
         textAnimation.style.color = 'black';
         textAnimation.style.backgroundColor = 'var(--third)';
     }, 300);
 
+    // Clears animation classes after animation is finished
     setTimeout(() => {
         textAnimation.classList.remove('text-animation');
         backgroundAnimation.classList.remove('background-animation');
@@ -49,27 +37,18 @@ console.log(textAnimation, backgroundAnimation);
         backgroundAnimation.classList.remove('reverse');
     }, 300);
 
-   
-
     });
 
+    // Animation for unhover begins. Button mouseleave listener
     button.addEventListener('mouseleave', () => {
         textAnimation.style = '';
         textAnimation.style = '';
-        console.log('exit');
         textAnimation.classList.add('text-animation');
         backgroundAnimation.classList.add('background-animation');
         textAnimation.classList.add('reverse');
         backgroundAnimation.classList.add('reverse');
         
-        // button.innerHTML = `
-        // <div class="main-button text-animation reverse">
-        //     <div class="button-overlay background-animation reverse">
-                
-        //     </div>
-        //     ${buttonText}
-        // </div>
-        // `
+        // Clears animation classes after animation is finished
         setTimeout(() => {
             textAnimation.classList.remove('text-animation');
             backgroundAnimation.classList.remove('background-animation');
