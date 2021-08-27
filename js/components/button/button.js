9f0cad4bbd6f3b69d6bc751fcfcd698b46e232e9
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-27 19:24:25 +0300

    Javascript animation fixes #3

--- js/components/button/button.js
+++ js/components/button/button.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('new-per-button');
 // const textAnimation = document.querySelector('.main-button');
-// const backgroundAnimation = document.querySelector('.');
+// const backgroundAnimation = document.querySelector('.button-overlay');
 let buttonText = button.innerHTML;
 
 function buttonAnimation() {
@@ -9,39 +9,74 @@ function buttonAnimation() {
 
     console.log(buttonText);
     button.innerHTML = `
-    <div class="main-button">
-        <div class="button-overlay">
+    <div id="main-button" class="main-button">
+        <div id="button-overlay" class="button-overlay">
             
         </div>
         ${buttonText}
     </div>
     `
+    const textAnimation = document.getElementById('main-button'); 
+    // querySelector('.main-button');
+    const backgroundAnimation = document.getElementById('button-overlay');
+    // querySelector('.button-overlay');
+console.log(textAnimation, backgroundAnimation);
+
 
     button.addEventListener('mouseenter', () => {
         console.log('enterdd');
-        
-        button.innerHTML = `
-        <div class="main-button text-animation">
-            <div class="button-overlay background-animation">
+        textAnimation.classList.add('text-animation');
+        backgroundAnimation.classList.add('background-animation');
+        // button.innerHTML = `
+        // <div class="main-button text-animation">
+        //     <div class="button-overlay background-animation">
                 
-            </div>
-            ${buttonText}
-        </div>
-        `
+        //     </div>
+        //     ${buttonText}
+        // </div>
+        // `
+
+
+    setTimeout(() => {
+        textAnimation.style.color = 'black';
+        textAnimation.style.backgroundColor = 'var(--third)';
+    }, 300);
+
+    setTimeout(() => {
+        textAnimation.classList.remove('text-animation');
+        backgroundAnimation.classList.remove('background-animation');
+        textAnimation.classList.remove('reverse');
+        backgroundAnimation.classList.remove('reverse');
+    }, 300);
+
+   
 
     });
 
     button.addEventListener('mouseleave', () => {
+        textAnimation.style = '';
+        textAnimation.style = '';
         console.log('exit');
+        textAnimation.classList.add('text-animation');
+        backgroundAnimation.classList.add('background-animation');
+        textAnimation.classList.add('reverse');
+        backgroundAnimation.classList.add('reverse');
         
-        button.innerHTML = `
-        <div class="main-button text-animation reverse">
-            <div class="button-overlay background-animation reverse">
+        // button.innerHTML = `
+        // <div class="main-button text-animation reverse">
+        //     <div class="button-overlay background-animation reverse">
                 
-            </div>
-            ${buttonText}
-        </div>
-        `
+        //     </div>
+        //     ${buttonText}
+        // </div>
+        // `
+        setTimeout(() => {
+            textAnimation.classList.remove('text-animation');
+            backgroundAnimation.classList.remove('background-animation');
+            textAnimation.classList.remove('reverse');
+            backgroundAnimation.classList.remove('reverse');
+        }, 300);
+
     });
 };
 
