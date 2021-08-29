9dbfd34b9339a74cb4cb71aa15b34940aecdf872
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-29 14:24:39 +0300

    Created responsive design both with CSS and Javascript #33

--- js/components/portfolio-gallery/GalleryNavigation.js
+++ js/components/portfolio-gallery/GalleryNavigation.js
@@ -12,8 +12,9 @@ class GalleryNavigation {
         this.link = link;
 
         this.listener();
-        this.clearClasses();
+        // this.clearClasses();
         this.resetGallery();
+        this.responsive();
         this.renderGallery();
 
     }
@@ -26,26 +27,21 @@ class GalleryNavigation {
                 switch (button.innerHTML) {
                     case 'All':
                         this.data = this.allData;
-                        // console.log('PASIRINKO ALL', this.data);
                         this.renderGallery('call all');
                         break;
                     case 'Brand':
                         this.data = this.brandData;
-                        // console.log('PASIRINKO BRAND', this.data);
                         this.renderGallery('call brand');
                         break;
                     case 'Design':
-                        console.log('PASIRINKO DESING');
                         this.data = this.designData;
                         this.renderGallery();
                         break;
                     case 'Photo':
-                        console.log('PASIRINKO PHOTO');
                         this.data = this.photoData;
                         this.renderGallery();
                         break;
                     case 'Video':
-                        console.log('PASIRINKO VIDEO');
                         this.data = this.videoData;
                         this.renderGallery();
                         break;
@@ -69,7 +65,6 @@ class GalleryNavigation {
 
 
         let i = 0;
-        console.log(this.data);
         for (const photo of this.data) {
             if (photo.status === 'published') {
                 const image = document.getElementById(`img${i + 1}`);
@@ -79,19 +74,9 @@ class GalleryNavigation {
                 i++;
    
             }
-
-            // photo.querySelector('.gallery-photo img') = this.link + this.data.imgName;
-            // photo.querySelector('.photo-title').innerHTML = this.data[i].overlayTitle;
-
-            // console.log(photo.querySelector('.gallery-photo img'));
-            // console.log(photo.querySelector('.photo-title').innerHTML);
-            // console.log(photo.que;
-            
         }
 
         this.animateGallery();
-        // const imgDOM = document.querySelector('.gallery-photo img');
-
     }
 
     resetGallery() {
@@ -108,6 +93,45 @@ class GalleryNavigation {
                 photo.classList.add('gallery-photo2');
             }, 100);
         }
+    }
+
+    responsive() {
+        
+        function clearStyles() {
+            for (let i = 1; i <= 8; i++) {
+                document.getElementById(`gal${i}`).style = '';
+            }
+        }
+
+        function fixMargins() {
+            setTimeout(() => {
+                if (window.innerWidth > 770) {
+                    clearStyles();
+                    document.getElementById(`gal4`).style = 'margin-top: -8%;';
+                    document.getElementById(`gal6`).style = 'margin-top: -8%;';
+                    document.getElementById(`gal8`).style = 'width: 65%;';
+                } 
+                else if (window.innerWidth <= 770 && window.innerWidth > 575) {
+                    clearStyles();
+                    document.getElementById(`gal3`).style = 'margin-top: -11%;';
+                    document.getElementById(`gal5`).style = 'margin-top: -22%;';
+                    document.getElementById(`gal7`).style = 'margin-top: -33%;';
+                } 
+                else if (window.innerWidth <= 575) {
+                    clearStyles();
+                } 
+            }, 50);
+        }
+        
+        fixMargins();
+
+
+        // 575
+        window.addEventListener('resize', () => {
+            setTimeout(() => {
+                fixMargins();
+            }, 500);
+        });
     }
 };
 
